f7d7fa86e3386dd23057b02e5f80fa3441db466e
lchen <lchenaction@gmail.com>
2015-02-15 12:29:44 -0500

    Initial Project

---
+++ java-web/src/main/java/lchen/action/App.java
@@ -0,0 +1,13 @@
+package lchen.action;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
